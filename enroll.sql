2a26c906d14ce51218d1392b5893d5ec6d18df94
voidking <voidking@qq.com>
2017-10-12 13:12:23 +0800

    sql文件添加注释

--- enroll.sql
+++ enroll.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50711
+Source Server         : laragon
+Source Server Version : 50505
 Source Host           : localhost:3306
 Source Database       : enroll
 
 Target Server Type    : MYSQL
-Target Server Version : 50711
+Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-05-02 16:55:23
+Date: 2017-10-12 13:11:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,14 +21,14 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `en_student`;
 CREATE TABLE `en_student` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `gender` varchar(2) NOT NULL,
-  `grade` varchar(32) DEFAULT NULL,
-  `education` varchar(32) DEFAULT NULL,
-  `school` varchar(32) DEFAULT NULL,
-  `institute` varchar(32) DEFAULT NULL,
-  `major` varchar(32) DEFAULT NULL,
-  `bus` varchar(2) DEFAULT NULL,
+  `name` varchar(32) NOT NULL COMMENT '姓名',
+  `gender` varchar(2) NOT NULL COMMENT '性别',
+  `grade` varchar(32) DEFAULT NULL COMMENT '年级',
+  `education` varchar(32) DEFAULT NULL COMMENT '学历',
+  `school` varchar(32) DEFAULT NULL COMMENT '学校',
+  `institute` varchar(32) DEFAULT NULL COMMENT '学院',
+  `major` varchar(32) DEFAULT NULL COMMENT '专业',
+  `bus` varchar(2) DEFAULT NULL COMMENT '是否需要校车',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
